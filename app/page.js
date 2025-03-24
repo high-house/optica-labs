22726d67ed6945b34fb24c2a4e6c879f06be89c9
high-haseeb <haseebkhalidoriginal@gmail.com>
2025-03-24 23:19:43 +0500

    changed font from noto-sans to geist

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState, useEffect } from 'react'
-import { IconArrowUp } from "@tabler/icons-react"; // Importing upward arrow icon
+import { IconArrowUp } from "@tabler/icons-react";
 import Landing from '@/components/Landing'
 import Team from '@/components/Team'
 import Quotes from '@/components/quotes'
@@ -10,41 +10,41 @@ import Resources from '@/components/resources'
 import ContactUs from '@/components/ContactUs'
 
 function Page() {
-  const [showButton, setShowButton] = useState(false);
+    const [showButton, setShowButton] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setShowButton(window.scrollY > window.innerHeight);
+    useEffect(() => {
+        const handleScroll = () => {
+            setShowButton(window.scrollY > window.innerHeight);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    return (
+        <div className="overflow-x-hidden font-geist">
+            {showButton && (
+                <button 
+                    className="w-12 z-[9999] h-12 fixed bottom-10 right-10 bg-[#52CBBE] text-white rounded-full flex items-center justify-center shadow-lg transition-all duration-300 hover:scale-110"
+                    onClick={scrollToTop}
+                >
+                    <IconArrowUp size={28} />
+                </button>
+            )}
 
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
-
-  return (
-    <div className="overflow-x-hidden">
-      {showButton && (
-        <button 
-          className="w-12 z-[9999] h-12 fixed bottom-10 right-10 bg-[#52CBBE] text-white rounded-full flex items-center justify-center shadow-lg transition-all duration-300 hover:scale-110"
-          onClick={scrollToTop}
-        >
-          <IconArrowUp size={28} />
-        </button>
-      )}
-
-      <Landing />
-      <Team />
-      <Quotes />
-      <Podcast />
-      <NewsArticles />
-      <Resources />
-      <ContactUs />
-    </div>
-  );
+            <Landing />
+            <Team />
+            <Quotes />
+            <Podcast />
+            <NewsArticles />
+            <Resources />
+            <ContactUs />
+        </div>
+    );
 }
 
 export default Page;
